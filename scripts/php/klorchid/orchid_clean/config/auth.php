91153c3538937d7ebd260c7d013febbf1d1f2240
lemyskaman <lemyskaman@gmail.com>
2021-04-24 06:04:34 -0400

    adding klorchid link and modifiying orchid config filis

--- scripts/php/klorchid/orchid_clean/config/auth.php
+++ scripts/php/klorchid/orchid_clean/config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => Kamansoft\Klorchid\Models\KlorchidUserModel::class,
         ],
 
         // 'users' => [
